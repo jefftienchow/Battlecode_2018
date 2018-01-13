30fd341790fece398363819debb7ac2d92d49ff7
Martin Winton <mwinton@mit.edu>
2018-01-13 17:53:15 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -34,8 +34,20 @@ while True:
 
     # frequent try/catches are a good idea
     try:
+
+
+        units = gc.get_team_array(Planet.Mars)
+
+        
+
         # walk through our units:
         for unit in gc.my_units():
+
+            x = random.randrange(0, 3, 1)
+
+
+
+
 
             # first, factory logic
             if unit.unit_type == bc.UnitType.Factory:
@@ -43,13 +55,32 @@ while True:
                 if len(garrison) > 0:
                     d = random.choice(directions)
                     if gc.can_unload(unit.id, d):
-                        print('unloaded a knight!')
+                        print('unloaded something')
                         gc.unload(unit.id, d)
                         continue
-                elif gc.can_produce_robot(unit.id, bc.UnitType.Knight):
-                    gc.produce_robot(unit.id, bc.UnitType.Knight)
-                    print('produced a knight!')
-                    continue
+
+
+
+
+                if x == 0:
+
+                    if gc.can_produce_robot(unit.id, bc.UnitType.Knight):
+                        gc.produce_robot(unit.id, bc.UnitType.Knight)
+                        print('produced a knight!')
+                        continue
+                elif x == 1:
+
+                    if gc.can_produce_robot(unit.id, bc.UnitType.Worker):
+                        gc.produce_robot(unit.id, bc.UnitType.Worker)
+                        print('produced a worker!')
+                        continue
+
+                elif x == 2:
+
+                    if gc.can_produce_robot(unit.id, bc.UnitType.Healer):
+                        gc.produce_robot(unit.id, bc.UnitType.Healer)
+                        print('produced a healer!')
+                        continue
 
             # first, let's look for nearby blueprints to work on
             location = unit.location
@@ -65,6 +96,10 @@ while True:
                         print('attacked a thing!')
                         gc.attack(unit.id, other.id)
                         continue
+                    if other.team == my_team and gc.is_heal_ready(unit.id) and gc.can_heal(unit.id,other.id):
+
+                        print('healed a thing')
+                        gc.heal(unit.id,other.id)
 
             # okay, there weren't any dudes around
             # pick a random direction:
