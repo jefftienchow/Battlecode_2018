16bc812d228291c0d5a43ca972d4a7077fffd1b0
mwinton1003 <mwinton@mit.edu>
2018-01-13 18:23:49 -0500

    Optimized Healers and Added Count
    notice me senpai

--- run.py
+++ run.py
@@ -34,8 +34,31 @@ while True:
 
     # frequent try/catches are a good idea
     try:
+
+
+        units = gc.my_units()
+        count = {}
+
+        count["healer"] = 0
+        count["knight"] = 0
+
+        for unit in units:
+
+            if unit.unit_type == bc.UnitType.Healer:
+
+                count["healer"]+=1
+
+            if unit.unit_type == bc.UnitType.Knight:
+                count["knight"] += 1
+
         # walk through our units:
         for unit in gc.my_units():
+
+            x = random.randrange(0, 3, 1)
+
+
+
+
 
             # first, factory logic
             if unit.unit_type == bc.UnitType.Factory:
@@ -43,13 +66,49 @@ while True:
                 if len(garrison) > 0:
                     d = random.choice(directions)
                     if gc.can_unload(unit.id, d):
-                        print('unloaded a knight!')
+                        print('unloaded something')
                         gc.unload(unit.id, d)
                         continue
-                elif gc.can_produce_robot(unit.id, bc.UnitType.Knight):
-                    gc.produce_robot(unit.id, bc.UnitType.Knight)
-                    print('produced a knight!')
-                    continue
+
+
+
+
+                if x == 0:
+
+                    if count["knight"] < count["healer"] and gc.can_produce_robot(unit.id, bc.UnitType.Knight):
+                        gc.produce_robot(unit.id, bc.UnitType.Knight)
+                        print('produced a knight!')
+                        continue
+                    else:
+
+                        if gc.can_produce_robot(unit.id, bc.UnitType.Healer):
+                            gc.produce_robot(unit.id, bc.UnitType.Healer)
+                            print('produced a healer!')
+                            continue
+
+
+
+                elif x == 1:
+
+                    if gc.can_produce_robot(unit.id, bc.UnitType.Worker):
+                        gc.produce_robot(unit.id, bc.UnitType.Worker)
+                        print('produced a worker!')
+                        continue
+
+
+
+
+                elif x == 2:
+
+                    if  count["healer"] < count["knight"]  and gc.can_produce_robot(unit.id, bc.UnitType.Healer):
+                        gc.produce_robot(unit.id, bc.UnitType.Healer)
+                        print('produced a healer!')
+                        continue
+
+                    elif gc.can_produce_robot(unit.id, bc.UnitType.Knight):
+                        gc.produce_robot(unit.id, bc.UnitType.Knight)
+                        print('produced a knight!')
+                        continue
 
             # first, let's look for nearby blueprints to work on
             location = unit.location
@@ -58,13 +117,17 @@ while True:
                 for other in nearby:
                     if unit.unit_type == bc.UnitType.Worker and gc.can_build(unit.id, other.id):
                         gc.build(unit.id, other.id)
-                        print('built a factory!')
+                        print('built a factory!!')
                         # move onto the next unit
                         continue
                     if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
                         print('attacked a thing!')
                         gc.attack(unit.id, other.id)
                         continue
+                    if other.team == my_team and gc.is_heal_ready(unit.id) and gc.can_heal(unit.id,other.id) and other.health <other.max_health:
+
+                        print('healed a thing')
+                        gc.heal(unit.id,other.id)
 
             # okay, there weren't any dudes around
             # pick a random direction:
